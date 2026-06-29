76b76b76df29f7ae8632d1ba90d6d9661bcce096
Asharani968 <asharanikarnal38@gmail.com>
2026-06-29 12:25:04 +0530

    Create project1_hello.py

---
+++ project1_hello.py
@@ -0,0 +1,6 @@
+# AI Foundation for Engineers - Project 1
+# My First Python File on GitHub
+
+name = "Asharani Karnal"
+print(f"Hello! Welcome to AI Foundation.")
+print("This is my first GitHub commit.")
